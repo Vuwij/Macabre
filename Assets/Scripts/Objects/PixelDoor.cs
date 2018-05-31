dc5372ac04715280920a580c59449a39b193db5f
Jason Wang <vuwij@me.com>
2018-05-30 21:32:22 -0400

    Church and Shack done

--- Assets/Scripts/Objects/PixelDoor.cs
+++ Assets/Scripts/Objects/PixelDoor.cs
@@ -8,5 +8,13 @@ namespace Objects
         public Vector2 dropOffLocation; // Specified in pixels
 
 		public Direction interactionDirection = Direction.All;
-    }
+
+		private void OnDrawGizmos()
+		{
+#if UNITY_EDITOR
+			Gizmos.color = Color.white;
+			Gizmos.DrawIcon(transform.position + (Vector3)dropOffLocation, "Destination");
+#endif
+		}
+	}
 }
