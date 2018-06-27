fe7d6ff4e9c9cfb5b8146dd721b5ade447842e78
Jason Wang <vuwij@me.com>
2018-06-27 00:32:26 -0400

    Fiixed matlab

--- Assets/Scripts/Environment/createmap.m
+++ Assets/Scripts/Environment/createmap.m
@@ -143,22 +143,5 @@ end
 
 h = imshow(FinishImage.img);
 
-function drawImageInLocation(x, y, z, imgHandle, finishImageHandle)
-    [l, w, ~] = size(finishImageHandle.img);
-    [la, wa, ~] = size(imgHandle.img);
-    
-    xloc = ceil(l) - ((x-1) + (y-1)) * 4 - ceil(la) - (z-1) * 8;
-    yloc = ceil(w/2) + ((x-1) - (y-1)) * 8 - ceil(wa/2);
-   
-    % Todo, better transparency
-    for i = 1:la
-        for j = 1:wa
-            if(imgHandle.tran(i,j) == 0) 
-                continue;
-            end
-            finishImageHandle.img(i+xloc,j+yloc,:) = imgHandle.img(i, j, :);
-        end
-    end
-end
 
 
