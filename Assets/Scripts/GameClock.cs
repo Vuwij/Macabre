8cdd29576cfcd2e76df4b3530fb6d43b6fee08a7
Jason Wang <vuwij@me.com>
2018-08-30 18:26:42 -0400

    Added more stuff to Time and Date

--- Assets/Scripts/GameClock.cs
+++ Assets/Scripts/GameClock.cs
@@ -9,20 +9,29 @@ public class GameClock
 {
 	public int totalSeconds = 0;
 
-	public enum MIndicator { AM, PM };
-	public enum DayOfWeek { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
-    public enum Month { January, February, March, April, May, June, July, August, September, October, November, December };
+	public enum MIndicator { AM = 0, PM = 1 };
+	public enum DayOfWeek { Monday = 0, Tuesday = 1, Wednesday = 2, Thursday = 3, Friday = 4, Saturday = 5, Sunday = 6 };
+	public enum Month { January = 0, February = 1, March = 2, April = 3, May = 4, June = 5, July = 6, August = 7, September = 8, October = 9, November = 10, December = 11 };
 
 	public int second => totalSeconds % 60;
 	public int minute => (totalSeconds / 60) % 60;
 	public int hour => (totalSeconds / 3600) % 60;
 	public int day => (totalSeconds / (24 * 36000) % 24);
 	public int week => (totalSeconds / (24 * 36000 * 7) % 7);
-	public int month => (totalSeconds / (24 * 36000 * 7 * 4) % 4);
+	public int monthnum => (totalSeconds / (24 * 36000 * 7 * 4) % 4);
 	public int year => (totalSeconds / (24 * 36000 * 7 * 52) % 52);
 	public MIndicator cycle => (MIndicator)(hour % 12);
 	public DayOfWeek dayOfWeek => (DayOfWeek)(day % 7);
+	public Month month => (Month)(monthnum % 12);
+	public string dayOfWeekString => dayOfWeek.ToString();
+	public string monthString => month.ToString();
     
+	public string formatTime(int time) {
+		if (time >= 10)
+			return time.ToString();
+		else return "0" + time.ToString();
+	}
+
     public void Tick()
     {
 		totalSeconds++;
@@ -30,7 +39,8 @@ public class GameClock
 		// Update UI
 		GameObject statsPanelObj = GameObject.Find("Stats Panel");
 		StatsPanel statsPanel = statsPanelObj.GetComponent<StatsPanel>();
-		statsPanel.Date = dayOfWeek.ToString() + " , month";
+		statsPanel.Date = "Week " + week.ToString() + ", " + dayOfWeekString;
+		statsPanel.Time = hour.ToString() + ":" + formatTime(minute) + ":" + formatTime(second);
 
 		// Lighting of the overworld
         //var backgroundobj = GameObject.Find("Background");
